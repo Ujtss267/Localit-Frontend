61bacbc1c1d0bb10b3a42ae28a513cc3db59fecf
Ujtss267 <ujtss267@gmail.com>
2025-08-08 13:12:07 +0900

    add source

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen grid place-items-center">
+      <button className="px-6 py-3 rounded-lg bg-blue-600 text-white hover:bg-blue-700">Tailwind OK?</button>
+      <p className="text-3xl text-red-500 mt-6">Hello Tailwind</p>
+    </div>
+  );
+}
